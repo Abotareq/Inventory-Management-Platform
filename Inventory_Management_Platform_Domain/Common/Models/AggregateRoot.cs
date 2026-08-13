3349c35b4bb2c6d1b6542813cc9a8c88047fecc6
Ahmad Tarek <96955833+Abotareq@users.noreply.github.com>
2026-08-13 23:32:43 +0300

    base models and interface for DDD

---
+++ Inventory_Management_Platform_Domain/Common/Models/AggregateRoot.cs
@@ -0,0 +1,28 @@
+﻿using Inventory_Management_Platform.Domain.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory_Management_Platform.Domain.Common.Models
+{
+    public abstract class AggregateRoot : Entity
+    {
+        private readonly List<IDomainEvent> _domainEvents = new();
+
+        protected AggregateRoot(Guid id) : base(id) { }
+
+        protected AggregateRoot() { }
+
+        public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+        protected void RaiseDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+
+        public void ClearDomainEvents()
+        {
+            _domainEvents.Clear();
+        }
+    }
+}
---
+++ Inventory_Management_Platform_Domain/Common/Models/Entity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory_Management_Platform.Domain.Common.Models
+{
+    internal class Entity
+    {
+    }
+}
