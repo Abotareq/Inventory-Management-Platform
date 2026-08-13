6e66bdfff9a98a702a91d9b3c475189d04e55294
Ahmad Tarek <96955833+Abotareq@users.noreply.github.com>
2026-08-14 02:25:20 +0300

    swagger configuratio

---
+++ Inventory_Management_Platform.Api/Extensions/SwaggerServiceExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.OpenApi;
+
+namespace Inventory_Management_Platform.Api.Extensions
+{
+    public static class SwaggerServiceExtensions
+    {
+        public static IServiceCollection AddSwaggerWithJwtAuth(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Inventory Management Platform API",
+                    Version = "v1"
+                });
+
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Enter your JWT access token below."
+                });
+
+                options.AddSecurityRequirement(document => new OpenApiSecurityRequirement
+                {
+                    [new OpenApiSecuritySchemeReference("Bearer", document)] = new List<string>()
+                });
+            });
+
+            return services;
+        }
+    }
+
+}
--- Inventory_Management_Platform.Api/Program.cs
+++ Inventory_Management_Platform.Api/Program.cs
@@ -1,34 +1,33 @@
+using Inventory_Management_Platform.Api.Extensions;
 using Inventory_Management_Platform.Application;
 using Inventory_Management_Platform.Infrastructure;
-
 using Inventory_Management_Platform.Infrastructure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddApplication();
     builder.Services.AddInfrastructure(builder.Configuration);
-
     builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
 
-
+    builder.Services.AddJwtAuthentication(builder.Configuration);
+    builder.Services.AddSwaggerWithJwtAuth();
 }
-
-// Add services to the container.
-
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     using var scope = app.Services.CreateScope();
     await RoleSeeder.SeedRolesAsync(scope.ServiceProvider);
     await TestUserSeeder.SeedTestUsersAsync(scope.ServiceProvider);
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
