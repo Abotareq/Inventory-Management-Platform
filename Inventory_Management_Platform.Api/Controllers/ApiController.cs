cb6fa551339445984dd393537dcba48f8b377202
Ahmad Tarek <96955833+Abotareq@users.noreply.github.com>
2026-08-14 02:29:00 +0300

    register endpoint with api controller for error driven design

---
+++ Inventory_Management_Platform.Api/Controllers/ApiController.cs
@@ -0,0 +1,49 @@
+﻿namespace Inventory_Management_Platform.Api.Controllers
+{
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        protected IActionResult Problem(List<Error> errors)
+        {
+            if (errors.Count == 0)
+            {
+                return Problem();
+            }
+
+            if (errors.All(e => e.Type == ErrorType.Validation))
+            {
+                return ValidationProblem(errors);
+            }
+
+            HttpContext.Items["errors"] = errors;
+
+            return Problem(errors[0]);
+        }
+
+        private IActionResult Problem(Error error)
+        {
+            var statusCode = error.Type switch
+            {
+                ErrorType.Conflict => StatusCodes.Status409Conflict,
+                ErrorType.Validation => StatusCodes.Status400BadRequest,
+                ErrorType.NotFound => StatusCodes.Status404NotFound,
+                ErrorType.Unauthorized => StatusCodes.Status401Unauthorized,
+                ErrorType.Forbidden => StatusCodes.Status403Forbidden,
+                _ => StatusCodes.Status500InternalServerError
+            };
+
+            return Problem(statusCode: statusCode, title: error.Description);
+        }
+        private IActionResult ValidationProblem(List<Error> errors)
+        {
+            var modelStateDictionary = new ModelStateDictionary();
+
+            foreach (var error in errors)
+            {
+                modelStateDictionary.AddModelError(error.Code, error.Description);
+            }
+
+            return ValidationProblem(modelStateDictionary);
+        }
+    }
+}
---
+++ Inventory_Management_Platform.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,39 @@
+﻿using ErrorOr;
+using Inventory_Management_Platform.Application.Authintication.Commands.Register;
+using Inventory_Management_Platform.Contracts.Authentication;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inventory_Management_Platform.Api.Controllers
+{
+
+    [Route("api/auth")]
+    public sealed class AuthenticationController : ApiController
+    {
+        private readonly ISender _mediator;
+
+        public AuthenticationController(ISender mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("register")]
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> Register(RegisterRequest request)
+        {
+            var command = new RegisterCommand(
+                request.FullName,
+                request.Email,
+                request.Password,
+                request.Role);
+
+            ErrorOr<RegisterResponse> registerResult = await _mediator.Send(command);
+
+            return registerResult.Match(
+                response => Ok(response),
+                errors => Problem(errors));
+        }
+    }
+
+}
