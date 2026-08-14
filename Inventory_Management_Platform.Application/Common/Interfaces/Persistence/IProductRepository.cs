42dad970d10a9e1ffe20de19ae39ccde68e8df55
Ahmad Tarek <96955833+Abotareq@users.noreply.github.com>
2026-08-15 02:53:09 +0300

    feat(product): add Product repo interface

---
+++ Inventory_Management_Platform.Application/Common/Interfaces/Persistence/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using Inventory_Management_Platform.Domain.Product;
+using Inventory_Management_Platform.Domain.Product.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory_Management_Platform.Application.Common.Interfaces.Persistence
+{
+    public interface IProductRepository
+    {
+        Task<Product?> GetByIdAsync(ProductId id);
+        Task<List<Product>> GetAllAsync();
+        Task AddAsync(Product product);
+        void Update(Product product);
+        Task<bool> ExistsBySkuAsync(string sku);
+    }
+}
