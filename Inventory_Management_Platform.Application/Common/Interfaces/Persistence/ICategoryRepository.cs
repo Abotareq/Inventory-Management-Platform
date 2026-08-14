599168302eef6fe797aebd62538459ef23af1cc4
Ahmad Tarek <96955833+Abotareq@users.noreply.github.com>
2026-08-14 18:13:47 +0300

    Category abstaction

---
+++ Inventory_Management_Platform.Application/Common/Interfaces/Persistence/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using Inventory_Management_Platform.Domain.Category.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory_Management_Platform.Application.Common.Interfaces.Persistence
+{
+    public interface ICategoryRepository
+    {
+        Task<Category?> GetByIdAsync(CategoryId id);
+        Task<List<Category>> GetAllAsync();
+        Task AddAsync(Category category);
+        void Update(Category category);
+        Task<bool> ExistsAsync(CategoryId id);
+    }
+}
