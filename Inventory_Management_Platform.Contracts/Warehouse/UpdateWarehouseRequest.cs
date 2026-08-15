8433a173dd4a1d8fd70588683ac59855c5595e5a
Ahmad Tarek <96955833+Abotareq@users.noreply.github.com>
2026-08-16 01:38:06 +0300

    feat(warehouse): add Warehouse-contracts

---
+++ Inventory_Management_Platform.Contracts/Warehouse/UpdateWarehouseRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory_Management_Platform.Contracts.Warehouse
+{
+    public sealed record UpdateWarehouseRequest(
+         string Name,
+         string Location);
+}
