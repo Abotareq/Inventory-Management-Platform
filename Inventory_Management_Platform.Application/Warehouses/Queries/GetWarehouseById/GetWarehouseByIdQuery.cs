2d9d38e09244db40e3463751fae66a482dd3dc93
Ahmad Tarek <96955833+Abotareq@users.noreply.github.com>
2026-08-16 01:52:00 +0300

    feat(warehouse): add Warehouse-getById :Queryhandler+endpoint

---
+++ Inventory_Management_Platform.Application/Warehouses/Queries/GetWarehouseById/GetWarehouseByIdQuery.cs
@@ -0,0 +1,12 @@
+﻿using ErrorOr;
+using Inventory_Management_Platform.Contracts.Warehouse;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory_Management_Platform.Application.Warehouses.Queries.GetWarehouseById
+{
+    public sealed record GetWarehouseByIdQuery(Guid WarehouseId) : IRequest<ErrorOr<WarehouseResponse>>;
+
+}
---
+++ Inventory_Management_Platform.Application/Warehouses/Queries/GetWarehouseById/GetWarehouseByIdQueryHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory_Management_Platform.Application.Warehouses.Queries.GetWarehouseById
+{
+    internal class GetWarehouseByIdQueryHandler
+    {
+    }
+}
