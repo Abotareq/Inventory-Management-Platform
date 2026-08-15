bf8c7c6adeaa3a462cc77e423d9edc4a4af102cf
Ahmad Tarek <96955833+Abotareq@users.noreply.github.com>
2026-08-16 01:29:14 +0300

    feat(warehouse): add Warehouse-repo interface

---
+++ Inventory_Management_Platform.Application/Common/Interfaces/Persistence/IWarehouseRepository.cs
@@ -0,0 +1,17 @@
+﻿using Inventory_Management_Platform.Domain.Warehouse;
+using Inventory_Management_Platform.Domain.Warehouse.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory_Management_Platform.Application.Common.Interfaces.Persistence
+{
+    public interface IWarehouseRepository
+    {
+        Task<Warehouse?> GetByIdAsync(WarehouseId id);
+        Task<List<Warehouse>> GetAllAsync();
+        Task AddAsync(Warehouse warehouse);
+        void Update(Warehouse warehouse);
+        Task<bool> ExistsAsync(WarehouseId id);
+    }
+}
