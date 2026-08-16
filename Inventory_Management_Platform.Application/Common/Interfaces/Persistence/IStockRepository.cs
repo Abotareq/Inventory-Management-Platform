2cedab89302f71529abc79157e699b83a47492ee
Ahmad Tarek <96955833+Abotareq@users.noreply.github.com>
2026-08-16 21:22:20 +0300

    feat(stock): add Stock repo interface

---
+++ Inventory_Management_Platform.Application/Common/Interfaces/Persistence/IStockRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory_Management_Platform.Application.Common.Interfaces.Persistence
+{
+    internal class IStockRepository
+    {
+    }
+}
--- Inventory_Management_Platform_Domain/Stock/Events/StockQuntityChange.cs
+++ Inventory_Management_Platform_Domain/Stock/Events/StockQuntityChange.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using Inventory_Management_Platform.Domain.Common.Interfaces;
+using Inventory_Management_Platform.Domain.Stock.ValueObjects;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Inventory_Management_Platform.Domain.Stock.Events
 {
-    internal class StockIncreased
-    {
-    }
+    public sealed record StockIncreased(
+       StockId StockId,
+       int Delta,
+       int ResultingQuantity,
+       string Reason,
+       Guid PerformedByUserId,
+       DateTime Timestamp) : IDomainEvent;
+
+    public sealed record StockDecreased(
+        StockId StockId,
+        int Delta,
+        int ResultingQuantity,
+        string Reason,
+        Guid PerformedByUserId,
+        DateTime Timestamp) : IDomainEvent;
 }
